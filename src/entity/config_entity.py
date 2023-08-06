09c32c480943630b6a51cd06d007abc399d451b5
shivan <kshivan848@gmail.com>
2023-08-06 09:44:38 +0530

    data validation completed

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -3,3 +3,10 @@ from collections import namedtuple
 DataIngestionConfig = namedtuple("DatasetConfig", ['dataset_download_url',
                                                    'raw_data_dir',
                                                    'ingested_dir'])
+
+
+DataValidationConfig = namedtuple("DataValidationConfig",['clean_data_dir',
+                                                          'objects_dir',
+                                                          'books_csv_files',
+                                                          'ratings_csv_file'])
+
